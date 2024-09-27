f279e8aa1d67a112f0116b071c2040dc5e6d87f2
Ruhena Akther <aktherruhena17@gmail.com>
2024-09-28 00:02:17 +0600

    new commit app1

---
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.assignment1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.loginButton);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+    }
+}
